722ad55246c3716f51b1c398cfdd09346c85c1c2
Markus Kauppila <markus.kauppila@gmail.com>
2022-06-12 21:09:53 +0300

    Add simple pprof support

--- cmd/server.go
+++ cmd/server.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
+
+	_ "net/http/pprof"
 
 	"github.com/mkauppila/mud/internal/game"
 	"github.com/mkauppila/mud/internal/server"
 )
+
+
+func setupPprof(host string, port int16) {
+	fmt.Printf("Start pprof at %s:%d\n", host, port)
+
+	// pprof is by default added to DefaultServerMux
+	err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil)
+	// err := http.ListenAndServe("localhost:5000", nil)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	exitC := make(chan struct{})
@@ -25,6 +40,8 @@ func main() {
 	server := server.NewServer(server.UuidGenerator, world)
 	go server.StartAcceptingConnections()
 	go world.RunGameLoop()
+
+	go setupPprof("localhost", 8000)
 
 	<-exitC
 
